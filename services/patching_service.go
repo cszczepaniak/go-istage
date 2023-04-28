ba3d8551e66ffaf47ea641cd0f54b4b510599535
cszczepaniak <cszczepaniak@gmail.com>
2023-04-28 10:08:59 -0500

    add patching service

---
+++ services/patching_service.go
@@ -0,0 +1,44 @@
+package services
+
+import (
+	"errors"
+
+	"github.com/cszczepaniak/go-istage/patch"
+)
+
+type PatchingService struct {
+	gs *GitService
+	ds *DocumentService
+}
+
+func NewPatchingService(gs *GitService, ds *DocumentService) *PatchingService {
+	return &PatchingService{
+		gs: gs,
+		ds: ds,
+	}
+}
+
+func (ps *PatchingService) ApplyPatch(dir patch.Direction, entireHunk bool, selectedLine int) error {
+	if ps.ds.viewFiles {
+		return errors.New(`ApplyPatch: viewFiles unimplemented`)
+	}
+
+	doc := ps.ds.Document
+	line := doc.Lines[selectedLine]
+
+	if line.Kind != patch.AdditionLine && line.Kind != patch.RemovalLine {
+		return nil
+	}
+
+	var lines []int
+	if entireHunk {
+		return errors.New(`ApplyPatch: entireHunk unimplemented`)
+	} else {
+		lines = []int{selectedLine}
+	}
+
+	// TODO compute patch with lines
+	_ = lines
+	return errors.New(`ApplyPatch: computing patch isn't implemented yet`)
+	return ps.gs.ApplyPatch(``, dir)
+}
