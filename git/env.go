59972bd1564fc274757edea7e1475c8661cdd7ff
Connor Szczepaniak <cszczepaniak@gmail.com>
2023-11-11 12:53:12 -0600

    test nolibgit instead, and remove the old impls

--- git/env.go
+++ git/env.go
@@ -1,12 +1,6 @@
 package git
 
 import (
-	"errors"
-	"os"
-	"path"
-	"path/filepath"
-	"strings"
-
 	"github.com/cszczepaniak/go-istage/nolibgit"
 )
 
@@ -15,83 +9,9 @@ type Environment struct {
 	pathToGit string
 }
 
-func NewEnvironment(repoPath string, pathToGit string) (Environment, error) {
-	if pathToGit == `` {
-		var err error
-		pathToGit, err = resolveGitPath()
-		if err != nil {
-			return Environment{}, err
-		}
-	}
-	if repoPath == `` {
-		var err error
-		repoPath, err = resolveRepoPath()
-		if err != nil {
-			return Environment{}, err
-		}
-	}
-	return Environment{
-		repoPath:  repoPath,
-		pathToGit: pathToGit,
-	}, nil
-}
-
 func newEnvironmentFromNoLibGit(env nolibgit.Environment) Environment {
 	return Environment{
 		repoPath:  env.RepoDir,
 		pathToGit: env.GitExecutable,
 	}
-}
-
-func resolveGitPath() (string, error) {
-	path := os.Getenv("PATH")
-	if path == `` {
-		return ``, nil
-	}
-
-	paths := strings.Split(path, string(os.PathListSeparator))
-	lookingFor := []string{"git", "git.exe"}
-
-	var res string
-	for _, p := range paths {
-		for _, exe := range lookingFor {
-			res = filepath.Join(p, exe)
-			_, err := os.Stat(res)
-			if err != nil {
-				if os.IsNotExist(err) {
-					continue
-				}
-				return ``, err
-			}
-
-			return res, nil
-		}
-	}
-	return ``, errors.New(`could not find git`)
-}
-
-func resolveRepoPath() (string, error) {
-	d, err := os.Getwd()
-	if err != nil {
-		return ``, err
-	}
-
-	return resolveRepoPathFrom(d)
-}
-
-func resolveRepoPathFrom(start string) (string, error) {
-	entries, err := os.ReadDir(start)
-	if err != nil {
-		return ``, err
-	}
-	for _, e := range entries {
-		if e.IsDir() && strings.HasSuffix(e.Name(), `.git`) {
-			path, err := filepath.Abs(filepath.Join(start, e.Name()))
-			if err != nil {
-				return ``, err
-			}
-			return path, nil
-		}
-	}
-	return resolveRepoPathFrom(path.Join(`..`, start))
 }
