c4099806ae2385b630d532b0a8a898a8ee8bb54b
Connor Szczepaniak <cszczepaniak@gmail.com>
2023-11-11 14:11:54 -0600

    add tests

--- git/exec_integration_test.go
+++ git/exec_integration_test.go
@@ -1,9 +1,11 @@
 package git
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/cszczepaniak/go-istage/nolibgit"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -18,4 +20,38 @@ func TestExec(t *testing.T) {
 
 	err = gs.Exec(`status`).Run()
 	require.NoError(t, err)
+}
+
+func TestExecWithStdout(t *testing.T) {
+	NewTestRepo(t)
+
+	env, err := nolibgit.LoadEnvironment()
+	require.NoError(t, err)
+
+	gs, err := NewClient(env)
+	require.NoError(t, err)
+
+	sb := &strings.Builder{}
+
+	err = gs.Exec(`status`).WithStdout(sb).Run()
+	require.NoError(t, err)
+
+	assert.Equal(t, "On branch master\nnothing to commit, working tree clean\n", sb.String())
+}
+
+func TestExecWithStderr(t *testing.T) {
+	NewTestRepo(t)
+
+	env, err := nolibgit.LoadEnvironment()
+	require.NoError(t, err)
+
+	gs, err := NewClient(env)
+	require.NoError(t, err)
+
+	sb := &strings.Builder{}
+
+	err = gs.Exec(`what`).WithStderr(sb).Run()
+	assert.Error(t, err)
+
+	assert.Equal(t, "git: 'what' is not a git command. See 'git --help'.\n\nThe most similar command is\n\tmktag\n", sb.String())
 }
