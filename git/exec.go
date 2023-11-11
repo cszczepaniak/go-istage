7f214a9c7ee84b7828710ec0ce5b23ddbcabdeb4
Connor Szczepaniak <cszczepaniak@gmail.com>
2023-11-11 14:10:39 -0600

    allow specifying stdout and stderr

--- git/exec.go
+++ git/exec.go
@@ -16,9 +16,11 @@ type GitExecBuilder struct {
 	env  nolibgit.Environment
 	repo *git.Repository
 
-	stdin      io.Reader
+	stdin  io.Reader
+	stdout io.Writer
+	stderr io.Writer
+
 	updateRepo bool
-	capture    bool
 	args       []string
 }
 
@@ -27,8 +29,10 @@ func (gs *Client) Exec(name string) *GitExecBuilder {
 		env:  gs.env,
 		repo: gs.repo,
 
+		stdout: io.Discard,
+		stderr: io.Discard,
+
 		updateRepo: true,
-		capture:    true,
 		args:       []string{name},
 	}
 }
@@ -38,8 +42,10 @@ func Exec(env nolibgit.Environment, repo *git.Repository, name string) *GitExecB
 		env:  env,
 		repo: repo,
 
+		stdout: io.Discard,
+		stderr: io.Discard,
+
 		updateRepo: true,
-		capture:    true,
 		args:       []string{name},
 	}
 }
@@ -49,13 +55,18 @@ func (eb *GitExecBuilder) WithStdin(r io.Reader) *GitExecBuilder {
 	return eb
 }
 
-func (eb *GitExecBuilder) SkipUpdate() *GitExecBuilder {
-	eb.updateRepo = false
+func (eb *GitExecBuilder) WithStdout(w io.Writer) *GitExecBuilder {
+	eb.stdout = w
 	return eb
 }
 
-func (eb *GitExecBuilder) SkipCapture() *GitExecBuilder {
-	eb.capture = false
+func (eb *GitExecBuilder) WithStderr(w io.Writer) *GitExecBuilder {
+	eb.stderr = w
+	return eb
+}
+
+func (eb *GitExecBuilder) SkipUpdate() *GitExecBuilder {
+	eb.updateRepo = false
 	return eb
 }
 
@@ -69,10 +80,8 @@ func (eb *GitExecBuilder) Run() error {
 	cmd.Dir = eb.env.WorkingDir
 
 	var out strings.Builder
-	if eb.capture {
-		cmd.Stdout = &out
-		cmd.Stderr = &out
-	}
+	cmd.Stdout = io.MultiWriter(&out, eb.stdout)
+	cmd.Stderr = io.MultiWriter(&out, eb.stderr)
 	if eb.stdin != nil {
 		cmd.Stdin = eb.stdin
 	}
@@ -82,14 +91,12 @@ func (eb *GitExecBuilder) Run() error {
 		return fmt.Errorf("error executing %+v:\n%s", eb.args, out.String())
 	}
 
-	if eb.capture {
-		output := out.String()
-		sc := bufio.NewScanner(strings.NewReader(output))
-		for sc.Scan() {
-			txt := sc.Text()
-			if strings.HasPrefix(txt, `fatal:`) || strings.HasPrefix(txt, `error:`) {
-				return fmt.Errorf("error executing %+v:\n%s", eb.args, output)
-			}
+	output := out.String()
+	sc := bufio.NewScanner(strings.NewReader(output))
+	for sc.Scan() {
+		txt := sc.Text()
+		if strings.HasPrefix(txt, `fatal:`) || strings.HasPrefix(txt, `error:`) {
+			return fmt.Errorf("error executing %+v:\n%s", eb.args, output)
 		}
 	}
 
